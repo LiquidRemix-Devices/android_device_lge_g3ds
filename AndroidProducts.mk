e91ef1538bddcdeff88becd74026fed17ef30267
Aleksander <johnnylittleplanet@gmail.com>
2018-05-06 22:48:32 +0200

    g3: initial liquid g3 [2/2]

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,6 @@
 #
-# Copyright 2016 The CyanogenMod Project
+# Copyright (C) 2018 The LineageOS Open Source Project
+# Copyright (C) 2018 The Liquid Remix Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,15 +15,5 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit from g3ds device
-$(call inherit-product, device/lge/g3ds/device.mk)
-
-# Set those variables here to overwrite the inherited values.
-PRODUCT_DEVICE := g3ds
-PRODUCT_NAME := full_g3ds
-PRODUCT_BRAND := lge
-PRODUCT_MODEL := g3ds
-PRODUCT_MANUFACTURER := LGE
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/liquid.mk
--- lineage.mk
+++
@@ -1,9 +0,0 @@
-$(call inherit-product, device/lge/g3ds/full_g3ds.mk)
-
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-PRODUCT_NAME := lineage_g3ds
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="g3"
